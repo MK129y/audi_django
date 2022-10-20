11293cf66c6a67e0013863b33992f789316e310c
MmM1 <ivanov1088w@gmail.com>
2022-10-20 06:59:21 +0300

    add File

---
+++ webexemple/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+#
+# def index(request):
+#     return HttpResponse('<h>Привет мир</h>')
+
+def webexemple(request):
+    return HttpResponse('<h>Привет iiiiмир</h>')
